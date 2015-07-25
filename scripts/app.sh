abf7d0a7d952e2b4cbc07e6325590e494d78b7f6
Thien <fcduythien@gmail.com>
2015-07-25 17:47:19 +0700

    fix app.sh

--- scripts/app.sh
+++ scripts/app.sh
@@ -4,6 +4,6 @@ echo ">>Install composer"
 cd "${ROOT_DIR}"
 php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 cp common/config/config.example.php common/config/config.php
-chmod 777 -R apps/logs
+#chmod 777 -R apps/logs
 chmod 777 -R apps/cache/volt
 
